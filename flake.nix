da021d14f1318a58b2c1b2d7243de04d9d57a9a7
Hongrui Fang <chfanghr@gmail.com>
2023-07-11 19:24:39 +0800

    make it compile on nixos

---
+++ flake.nix
@@ -0,0 +1,58 @@
+{
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.nci.url = "github:yusdacra/nix-cargo-integration";
+  inputs.nci.inputs.nixpkgs.follows = "nixpkgs";
+  inputs.parts.url = "github:hercules-ci/flake-parts";
+  inputs.parts.inputs.nixpkgs-lib.follows = "nixpkgs";
+
+  outputs = inputs @ {
+    parts,
+    nci,
+    ...
+  }:
+    parts.lib.mkFlake {inherit inputs;} {
+      systems = ["x86_64-linux"];
+      imports = [nci.flakeModule];
+      perSystem = {
+        config,
+        pkgs,
+        lib,
+        ...
+      }: let
+        # shorthand for accessing outputs
+        # you can access crate outputs under `config.nci.outputs.<crate name>` (see documentation)
+        outputs = config.nci.outputs;
+      in {
+        # declare projects
+        # relPath is the relative path of a project to the flake root
+        # TODO: change this to your crate's path
+        nci.projects."radixdlt-scrypto" = {
+          relPath = "simulator";
+          # export all crates (packages and devshell) in flake outputs
+          # alternatively you can access the outputs and export them yourself
+          export = true;
+        };
+        # configure crates
+        nci.crates = {
+          "simulator" = {
+            # look at documentation for more options
+          };
+        };
+
+        devShells.default = outputs."radixdlt-scrypto".devShell.overrideAttrs (old: {
+          packages = (old.packages or []) ++ [pkgs.cmake];
+          nativeBuildInputs =
+            (old.nativeBuildinputs or [])
+            ++ (with pkgs; [
+              llvmPackages.libclang
+              llvmPackages.libcxxClang
+              clang
+            ]);
+          LIBCLANG_PATH = "${pkgs.llvmPackages.libclang.lib}/lib";
+          BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.llvmPackages.libclang.lib}/lib/clang/${lib.getVersion pkgs.clang}/include";
+        });
+
+        packages.default = outputs."radixdlt-scrypto".packages.release;
+      };
+    };
+}
